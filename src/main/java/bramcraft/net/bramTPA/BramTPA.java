5aa345344f10b6874c4d78315f7e4c09eb42858e
bramDVD <bram.wijnen@bramcraft.net>
2025-04-17 14:11:02 +0200

    Initial commit

---
+++ src/main/java/bramcraft/net/bramTPA/BramTPA.java
@@ -0,0 +1,17 @@
+package bramcraft.net.bramTPA;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BramTPA extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/bramcraft/net/bramTPA/TpaListener.java
@@ -0,0 +1,4 @@
+package bramcraft.net.bramTPA;
+
+public class TpaListener {
+}
